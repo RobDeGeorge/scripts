0cab9c11b5749f4b76f93123d9e03d42cac66e16
RobDeGeorge <rob.degeorge@gmail.com>
2025-08-02 08:35:38 -0700

    added config files and a copier script

---
+++ sync-configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copy config files to current directory
+cp ~/.config/i3/config ./i3-config
+cp ~/.config/i3blocks/config ./i3blocks-config
+cp ~/.config/dunst/dunstrc ./dunstrc
+cp ~/.config/kitty/kitty.conf ./kitty.conf
+
+echo "Config files synced to current directory"
